716b41dcd0df09232e9fe055819314943ec8633f
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2024-11-19 18:36:29 +0000

    update Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,8 +3,15 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
+        
         <nav className="navbar">
-            <div>Navbar</div>
+            <a href="#" className="navbar-brand">
+                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="12" cy="12" r="10" fill="currentColor" />
+                </svg>
+                <span className="navbar-title">Navbar</span>
+            </a>
+            
         </nav>
     )
 }
