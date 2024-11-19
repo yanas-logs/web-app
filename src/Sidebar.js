8e4aa1d3eee311a811e4f398c43d702f45a815e6
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2024-11-19 18:19:06 +0000

    update Sidebar.js and Sidebar.css

---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Sidebar.css';
+
+const Sidebar = () => {
+    return (
+        <nav className="navbar">
+            <div>Sidebar</div>
+        </nav>
+    )
+}
+
+export default Sidebar;
