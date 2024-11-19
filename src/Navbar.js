93a9110a1e669bb9dc169b6b6a8e039dc464c510
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2024-11-19 20:14:36 +0000

    update Navbar from sidebar branch

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,14 +5,29 @@ const Navbar = () => {
     return (
         
         <nav className="navbar">
+            <div>
             <a href="#" className="navbar-brand">
                 <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="12" cy="12" r="10" fill="currentColor" />
                 </svg>
                 <span className="navbar-title">Navbar</span>
             </a>
+            </div>
+
+            
+            <div className="menu">
+                <a href="#home">Home</a>
+                <a href="#about-us">About Us</a>
+            </div>
+            
+
             
         </nav>
+        <div className="menu">
+                <a href="#home">Home</a>
+                <a href="#about-us">About Us</a>
+            </div>
+
     )
 }
 
